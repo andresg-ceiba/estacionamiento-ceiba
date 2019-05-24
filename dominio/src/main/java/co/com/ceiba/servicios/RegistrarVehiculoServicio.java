2e782bedc25aaebbc5a5c02ee6a51fd7a90bdd50
Andres Gonzalez <andres.gonzalez@ceiba.com.co>
2019-05-24 14:53:52 -0500

    feat: validación de placa

--- dominio/src/main/java/co/com/ceiba/servicios/RegistrarVehiculoServicio.java
+++ dominio/src/main/java/co/com/ceiba/servicios/RegistrarVehiculoServicio.java
@@ -2,10 +2,14 @@ package co.com.ceiba.servicios;
 
 import co.com.ceiba.dominio.comun.ProveedorTiempo;
 import co.com.ceiba.dominio.comun.ValidadorArgumentos;
+import co.com.ceiba.dominio.comun.excepcion.ExcepcionOperacionNoPermitida;
 import co.com.ceiba.dominio.vehiculo.TipoVehiculo;
 import co.com.ceiba.dominio.vehiculo.Vehiculo;
 import co.com.ceiba.dominio.vehiculo.VehiculoRepositorio;
 import lombok.RequiredArgsConstructor;
+
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
 
 @RequiredArgsConstructor
 public class RegistrarVehiculoServicio {
@@ -13,6 +17,7 @@ public class RegistrarVehiculoServicio {
     private static final String PLACA_OBLIGATORIA = "La placa del vehículo es obligatoria";
     private static final String TIPO_OBLIGATORIO = "La placa es obligatoria";
     private static final String TIPO_INVALIDO = "El tipo de vehiculo no está permitido";
+    private static final String ENTRADA_NO_PERMITIDA = "Las placas iniciadas en 'A' solo pueden ingresar los días lunes y domingos";
 
     private final ValidadorArgumentos validador;
 
@@ -27,9 +32,20 @@ public class RegistrarVehiculoServicio {
         validador.validarObligatorio(tipoVehiculo, TIPO_OBLIGATORIO);
         validador.validarValorEnumValido(tipoVehiculo, TipoVehiculo.class, TIPO_INVALIDO);
 
-        Vehiculo vehiculoEntrante = new Vehiculo(TipoVehiculo.valueOf(tipoVehiculo), placa, proveedorTiempo.obtenerHoraActual());
+        LocalDateTime horaEntrada = proveedorTiempo.obtenerHoraActual();
+
+        validarEntradaPermitida(placa, horaEntrada);
+
+        Vehiculo vehiculoEntrante = new Vehiculo(TipoVehiculo.valueOf(tipoVehiculo), placa, horaEntrada);
 
         return repositorioVehiculos.registrar(vehiculoEntrante);
     }
 
+    private void validarEntradaPermitida(String placa, LocalDateTime horaEntrada) {
+        if (placa.charAt(0) == 'A'
+                && !(horaEntrada.getDayOfWeek().equals(DayOfWeek.SUNDAY) || horaEntrada.getDayOfWeek().equals(DayOfWeek.MONDAY))) {
+
+            throw new ExcepcionOperacionNoPermitida(ENTRADA_NO_PERMITIDA);
+        }
+    }
 }
---
+++ dominio/src/test/java/co/com/ceiba/servicios/RegistrarVehiculoServicioTest.java
@@ -0,0 +1,17 @@
+package co.com.ceiba.servicios;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RegistrarVehiculoServicioTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void ejecutar() {
+    }
+}
